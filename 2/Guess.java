1585680bd92c72564cee73b42e5f4059016e9635
Wu Haotian <whtsky@gmail.com>
2016-12-25 23:11:07 +0800

    rewrite Graph

--- 2/Guess.java
+++ 2/Guess.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 
 
+class A {
+	int target;
+	A() {
+		target = (int) (Math.random()*100+1);
+	}
+	
+	boolean correct(int guessed) {
+		if (guessed == target) {
+			return true;
+		}
+		if (guessed > target) {
+			System.out.println("大了！");
+		} else {
+			System.out.println("小了！");
+		}
+		return false;
+	}
+}
+
+
 public class Guess {
 	public static void main(String[] args) {
-		int target = (int) (Math.random()*100+1);
+		A a = new A();
 		int guessed;
-		Scanner input=new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("请输入一个在1~100之间的数字：");
-		while((guessed = input.nextInt()) != target) {
-			if (guessed > target) {
-				System.out.println("大了！");
-			} else {
-				System.out.println("小了！");
-			}
+		while(!a.correct(guessed = input.nextInt())) {
 			System.out.print("再试一下: ");
 		}
 		input.close();
